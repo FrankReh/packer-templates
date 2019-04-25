009d85b1a0a2444e4b2a4e74a562e8d2cf2a6e55
Frank Rehwinkel <frankrehwinkel@gmail.com>
2019-04-25 11:45:31 -0400

    Fix dragonflybsd cleanup to keep ca_root_nss
    Also remove two other packages and some groups and users.

--- dragonflybsd/cleanup.dragonflybsd.sh
+++ dragonflybsd/cleanup.dragonflybsd.sh
@@ -3,10 +3,18 @@
 # Accelerate boot
 echo 'autoboot_delay="0"' >>/boot/loader.conf
 
+# To avoid loosing this package when git is removed.
+pkg set -y -A 0 ca_root_nss &&
+
 # Remove non-critical packages and clear cache
-pkg remove -y bind-tools cdrtools git &&
-    pkg autoremove -y
+pkg remove -y wpa_supplicant isc-dhcp44-server bind-tools cdrtools git &&
+    pkg autoremove -y &&
     pkg clean -y
+
+# Remove some groups and users.
+pw groupdel dhcpd
+pw groupdel git_daemon
+pw userdel _dhcpd
 
 # Clear LiveCD files
 rm -rf /README* /autorun /index.html /dflybsd.ico /etc.hdd /boot.catalog
